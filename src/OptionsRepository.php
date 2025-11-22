7db823cbdb30baadb70c967b42608bd6d8c0a496
Bruno M. Noronha <36389316+BrunoMNoronha@users.noreply.github.com>
2025-11-22 14:07:43 -0300

    Centraliza chaves de configuracao e limpa exclusao

--- src/OptionsRepository.php
+++ src/OptionsRepository.php
@@ -6,6 +6,44 @@ namespace AutoPostAI;
 
 class OptionsRepository
 {
+    /**
+     * @var string[]
+     */
+    private const MAIN_OPTION_KEYS = [
+        'map_api_key',
+        'map_status',
+        'map_usar_imagens',
+        'map_tema',
+        'map_idioma',
+        'map_estilo',
+        'map_qtd_paragrafos',
+        'map_palavras_por_paragrafo',
+        'map_idioma2',
+        'map_estilo2',
+        'map_tom',
+        'map_request_timeout',
+        'map_max_tokens',
+        'map_modelo_ia',
+        'map_temperatura',
+        'map_gerar_imagem_auto',
+        'map_system_prompt',
+        'map_image_model',
+        'map_image_style',
+        'map_image_resolution',
+        'map_image_quality',
+        'map_seo_metadados',
+        'map_seo_tags_extra',
+    ];
+
+    /**
+     * @var string[]
+     */
+    private const AUTOMATION_OPTION_KEYS = [
+        'map_auto_publicar',
+        'map_auto_geracao',
+        'map_frequencia_cron',
+    ];
+
     private string $mainOptionGroup = 'map_ent_opcoes';
     private string $automationOptionGroup = 'map_ent_opcoes_automacao';
 
@@ -55,5 +93,29 @@ EOD;
         foreach ($keys as $key) {
             delete_option($key);
         }
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getMainOptionKeys(): array
+    {
+        return self::MAIN_OPTION_KEYS;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getAutomationOptionKeys(): array
+    {
+        return self::AUTOMATION_OPTION_KEYS;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getRegisteredOptionKeys(): array
+    {
+        return [...self::MAIN_OPTION_KEYS, ...self::AUTOMATION_OPTION_KEYS];
     }
 }
